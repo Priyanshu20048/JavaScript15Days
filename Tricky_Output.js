b9719414e63b692189757c0e733a0f1b6371bc50
Priyanshu20048 <mittal.priyanshu2004@gmail.com>
2026-01-30 15:27:27 +0530

    practicing output questions

---
+++ Tricky_Output.js
@@ -0,0 +1,34 @@
+    let name1 = "JavaScript";
+    name1[0] = "j";
+    console.log(name1); // JavaScript  
+
+    var a = 10;
+    var a = 20;
+    console.log(a); // 20
+
+    let a = 10;
+    let a = 20;
+    console.log(a); // error -> already been declared 
+
+    const x = 5;
+    x = 10;
+    console.log(x); // error -> Assignment to constant variable 
+
+    let str = "hello";
+    console.log(str.length); // 5
+
+    let s = "   js   ";
+    console.log(s.trim().length); // 2
+
+    let a = 10;
+    let b = 20;
+    console.log("Sum is " + a+b); // Sum is 1020
+    // one operand is string then concat 
+
+    let a = 10;
+    let b = 20;
+    console.log(`Sum is ${a+b}`); // Sum is 30
+
+
+
+
